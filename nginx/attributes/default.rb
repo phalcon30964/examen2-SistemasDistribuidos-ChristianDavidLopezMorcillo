6a8242ab77b198227846d82f8bfe1dd453e061f3
Christian David Lopez Morcillo <cdlopezmorcillo@gmail.com>
2017-05-07 19:40:14 -0500

    Update

--- nginx/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default[:nginx][:ipweb1]='192.168.131.126'
-default[:nginx][:ipweb2]='192.168.131.127'
-default[:nginx][:puerto_nginx]='8080'
--- web/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default[:web][:usuarioweb_web]='icesi'
-default[:web][:ip_web]='192.168.131.125'
-default[:web][:passwordweb_web]='12345'
